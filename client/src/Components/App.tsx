1dbb5339b2a0d375bb34232ba043e072b36c91ec
Loqui67 <loicwalter88@gmail.com>
2022-07-31 16:57:43 +0200

    carousel

---
+++ client/src/Components/App.tsx
@@ -0,0 +1,11 @@
+import CarouselComponent from "./CarouselComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <CarouselComponent />
+    </div>
+  );
+}
+
+export default App;
