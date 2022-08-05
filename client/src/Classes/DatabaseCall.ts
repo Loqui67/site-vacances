9a61a739ce3734a5ed8e3f3633b6e3c7a7fdc269
Loqui67 <loicwalter88@gmail.com>
2022-08-05 18:59:53 +0200

    base pr la database

--- client/src/Classes/DatabaseCall.ts
+++ client/src/Classes/DatabaseCall.ts
@@ -1,3 +1,10 @@
+import Axios from "axios";
+
 export class DatabaseCall {
 
+
+    getUsers = async () => {
+        const response = await Axios.get("http://localhost:3001/users");
+        return response.data;
+    }
 }
