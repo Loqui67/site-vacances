318ca2e675d7f19a3013201df50ec13e13e7edc4
Loqui67 <loicwalter88@gmail.com>
2022-08-08 22:13:43 +0200

    standby

--- client/src/Classes/DatabaseCall.ts
+++ client/src/Classes/DatabaseCall.ts
@@ -8,4 +8,21 @@ export class DatabaseCall {
         const response = await Axios.get(`${serverAddress}/users`);
         return response.data;
     }
+
+    getUser = async (id: number) => {
+        const response = await Axios.get(`${serverAddress}/users/${id}`);
+        return response.data;
+    }
+
+    createUser = async (user: any) => {
+        const response = await Axios.post(`${serverAddress}/users`, user);
+        return response.data;
+    }
+
+    updateUser = async (id: number, user: any) => {
+        const response = await Axios.put(`${serverAddress}/users/${id}`, user);
+        return response.data;
+    }   
+
+    
 }
